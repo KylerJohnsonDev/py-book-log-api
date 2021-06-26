7b1ec171eada6e0b85caf2918c329860f6547acd
Kyler Johnson <kylerjohnsondev@gmail.com>
2021-06-26 09:08:38 -0400

    move db into extensions file and users into user model file

--- src/app.py
+++ src/app.py
@@ -1,44 +1,18 @@
 #Import required packages
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from datetime import date
+from extensions import db, ma
+from models.user import User
 
 # https://tutorialwithproject.com/flask-rest-api-crud-operations/
 
 # Init app
 app = Flask(__name__)
 
-# SqlAlchemy
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/kylerjohnson/databases/book_inventory/book_inventory.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True 
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
-# models
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    auth_id = db.Column(db.String)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    dob = db.Column(db.Date)
-    bio = db.Column(db.String)
-    city = db.Column(db.String)
-    state = db.Column(db.String)
-    country = db.Column(db.String)
-    is_active = db.Column(db.Boolean)
-    
-    def __init__(self, auth_id, first_name, last_name, dob, bio, city, state, country, is_active):
-        self.auth_id = auth_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.dob = dob
-        self.bio = bio
-        self.city = city
-        self.state = state
-        self.country = country
-        self.is_active = is_active
+db.init_app(app)
         
 class UserSchema(ma.Schema):
     class Meta:
---
+++ src/models/user.py
@@ -0,0 +1,25 @@
+from extensions import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    auth_id = db.Column(db.String)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    dob = db.Column(db.Date)
+    bio = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    country = db.Column(db.String)
+    is_active = db.Column(db.Boolean)
+    
+    def __init__(self, auth_id, first_name, last_name, dob, bio, city, state, country, is_active):
+        self.auth_id = auth_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.bio = bio
+        self.city = city
+        self.state = state
+        self.country = country
+        self.is_active = is_active
